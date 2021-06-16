ef4a2e13a0775987b7de748acf090970875c3b31
waroach <brent.rankin@gmail.com>
2021-06-16 14:12:32 -0700

    Finished VowelsCounter

--- Beginner/vowelsCounter/index-START.js
+++ Beginner/vowelsCounter/index-START.js
@@ -5,21 +5,33 @@ e.g vowelsCounter('anehizxcv') // will return 3
 
 
                                                     // An Iterative Approach
+// function vowelsCounter(text) {
+//     const vowels = ["a","e","i","o","u"]
+//     let count = 0
+//     for(let letter of text.toLowerCase()){
+//         if (vowels.includes(letter)){
+//             count++
+//         }
+//     }
+//     return count
+// }
+                                                    // Using Regular Expressions
 function vowelsCounter(text) {
-    const vowels = ["a","e","i","o","u"]
-    let count = 0
-    for(let letter of text.toLowerCase()){
-        if (vowels.includes(letter)){
-            count++
-        }
+    // Search text with Regex and store all matching instances 
+    let matchingInstances = text.match(/[aeiou]/gi)
+    // g stands for a global search which means that after finding the first match, it will 
+    // not start over from the beginning but continue searching from the end of the previous match.
+    // i stands for case insensitive search which makes the whole expression case-insensitive 
+    // (for example /xyz/i would match XyZ).
+
+    // Check if matching instances exist then calculate length
+    if (matchingInstances) {    
+        // Return number of vowels
+        return matchingInstances.length
+    } else {
+        return 0
     }
-    return count
 }
-                                                    //
-                                                    //
-                                                    //
-                                                    //
-    // ********** OR **********
 
 
 
