3e685dadc6393d55dda6d24b091da831a1d63f93
waroach <brent.rankin@gmail.com>
2021-06-16 14:49:30 -0700

    Finished the MaxRecurringChar

--- Beginner/maxRecurringChar/index-START.js
+++ Beginner/maxRecurringChar/index-START.js
@@ -4,11 +4,62 @@ e.g maxRecurringChar('aabacada') // will return 'a'
 */
 
 
-// 3 failed 1 passed lol
+                                                    // Character Mapping
+// function maxRecurringChar(text) {
+//     let charMap = {}
+
+//     for(let char of text){
+//         if(charMap.hasOwnProperty(char)) {
+//             charMap[char]++
+//         }else{
+//             charMap[char] = 1
+//         }
+//     }
+// }
+                                                    // For…in Iteration
 function maxRecurringChar(text) {
-    // Code goes here
+    let charMap = {}
+    let maxCharValue = 0
+    let maxChar = ''
+
+    for (let char of text) {
+        if (charMap.hasOwnProperty(char)) {
+            charMap[char]++
+        } else {
+            charMap[char] = 1
+        }
+    }
+
+    for (let char in charMap) {
+        if (charMap[char] > maxCharValue) {
+            maxCharValue = charMap[char]
+            maxChar = char
+        }
+    }
+
+    return maxChar
 }
+                                                    // Forming Arrays from the Character Map
+// function maxRecurringChar(text) {
+//     let charMap = {}
+//     let charArray =[]
+//     let vaulesArray = []
+//     let maxCharValue = 0
 
+//     for (let char of text) {
+//         if (charMap.hasOwnProperty(char)) {
+//             charMap[char]++
+//         } else {
+//             charMap[char] = 1
+//         }
+//     }
 
+//     charArray = Object.keys(charMap)
+//     vaulesArray = Object.values(charMap)
+//     maxCharValue = Math.max(...vaulesArray)
+
+//     return charArray[vaulesArray.indexOf(maxCharValue)]
+// }
+                                                    //
 
 module.exports = maxRecurringChar;
