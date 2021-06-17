6dcf7f2cec16a1ba87d250eb75c0dee7ef34e028
waroach <brent.rankin@gmail.com>
2021-06-17 12:10:31 -0700

    Finished chunkArray

--- Beginner/chunkArray/index-START.js
+++ Beginner/chunkArray/index-START.js
@@ -1,21 +1,56 @@
 /* 
- Given two or more arrays, write a function that combines
- their elements into one array without any repetition. 
- E.g  mergeArrays([1,2,3,3,3], [1,4,5,2]) // should return [1,2,3,4,5]
+    Given two or more arrays, write a function that combines
+    their elements into one array without any repetition. 
+    E.g  mergeArrays([1,2,3,3,3], [1,4,5,2]) // should return [1,2,3,4,5]
+*/
+/* 
+    Given two or more arrays, write a function that combines
+    their elements into one array without any repetition. 
+    E.g  mergeArrays([1,2,3,3,3], [1,4,5,2]) // should return [1,2,3,4,5]
 */
 
-// FAILED i think instructions are wrong. should be Create an ${array} of ${size}
+                                                    // Looping thru the array
+// function chunkArray(array, size) {
+//     let result = []
+//     for (value of array){
+//         let lastArray = result[result.length -1 ]
+//         if(!lastArray || lastArray.length == size){
+//             result.push([value])
+//         } else{
+//             lastArray.push(value)
+//         }
+//     }
+//     return result
+// }
+                                                    // Looping Through the Number of Chunks
+// function chunkArray(array, size) {
+//     let result = []
+//     let arrayCopy = [...array]
+//     while (arrayCopy.length > 0) {
+//         result.push(arrayCopy.splice(0, size))
+//     }
+//     return result
+// }
+                                                    // Using .slice()
 function chunkArray(array, size) {
-    //concat the two arrays into one
-    let arrCon = [...array, size]
-    // step thru arrCon
-    // place unigueitems into new Array
-    let uniqueItems = arrCon.filter((c, index) => {
-        return arrCon.indexOf(c) === index
-    })
-
-    return uniqueItems
+    let result = []
+    for (let i = 0; i < array.length; i += size) {
+        let chunk = array.slice(i, i + size)
+        result.push(chunk)
+    }
+    return result
 }
+                                                    // RECURSION
+// function chunkArray(array, size) {
+//     if(array.length <= size){
+//         return [array]
+//     }
+//     return [array.slice(0,size), ...chunkArray(array.slice(size), size)]
+// }
+
+
+module.exports = chunkArray
+
 
 
 module.exports = chunkArray
