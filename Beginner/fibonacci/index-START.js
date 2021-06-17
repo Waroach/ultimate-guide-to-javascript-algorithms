a9b857f9d1ccc52f7add6a21a36c6d42f5b151de
waroach <brent.rankin@gmail.com>
2021-06-17 13:34:26 -0700

    Fixed fibonacci

--- Beginner/fibonacci/index-START.js
+++ Beginner/fibonacci/index-START.js
@@ -4,17 +4,28 @@ where the sequence is:
     [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, …]
 */
 
-// 2 failed 1 passed. expect(fibonacci(20)).toEqual(10946)
-function fibonacci(n) {
-    let a=1,b=0,temp
-    while (n >= 0) {
-        temp=a
-        a = a + b
-        b = temp
-        n--
-        console.log(b)
-        return b
+                                                    // First Try
+// function fibonacci(n){
+//     let prev=0, curr=1,temp=0
+//     while (n>=0){
+//         temp = curr
+//         curr = prev + curr
+//         prev = temp
+//         n--
+//         console.log(curr)
+//         return curr
+//     }
+// }
+                                                    // Second Try
+function fibonacci(n){
+    let prev=0, curr=1,temp=0
+    for(i=0;i<n;i++){
+        temp = curr
+        curr = prev + curr
+        prev = temp
     }
+    console.log(curr)
+    return curr
 }
 
 module.exports = fibonacci
