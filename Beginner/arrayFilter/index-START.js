5204f22b3a8925c567ea2d0ef3405c4f16587e1d
waroach <brent.rankin@gmail.com>
2021-06-17 13:14:32 -0700

    Finished both whreIBelong and arrayFilter

---
+++ Beginner/arrayFilter/index-START.js
@@ -0,0 +1,23 @@
+// Return the first element from the given array that passes
+// the test specified by the provided function.
+// Both the array and function are passed into the
+// function as arguments as shown below:
+
+                                                    // An Imperative Approach
+function arrayFilter(arr, func) {
+    for (let elem of arr) {
+        if (func(elem)) {
+            return elem
+        }
+    }
+    return undefined
+}
+                                                    // Using .filter()
+// function arrayFilter(arr, func) {
+//     let filteredArray = arr.filter(func);
+//     return filteredArray[0] ? filteredArray[0] : undefined;
+// }
+                                                    // Using .find()
+// function arrayFilter(arr, func) {
+//     return arr.find(func)
+// }
--- Beginner/whereIBelong/index-START.js
+++ Beginner/whereIBelong/index-START.js
@@ -8,17 +8,57 @@ than 1(index 0), but less than 2(index 1).
 */
 
 
-// 2 failed 2 passed
+                                                    // First Tru
+// function whereIBelong(arr, num) {
+//    let i = 0
+//    for(currNumber of arr){
+//       if (num < currNumber){
+//          console.log(` I Belong here ${i}`)
+//          return i
+//       }
+//       i++
+//    }
+// }
+
+                                                    // Using a For-loop to find the immediate larger value's position
+// function whereIBelong(arr, num) {
+//    arr.sort((a, b) => {
+//        return a - b
+//    })
+//    for (var i = 0; i < arr.length; i++) {
+//        if (arr[i] >= num) {            return i
+//        }
+//    }
+//    return arr.length
+// }
+                                                    // Using a For-loop to determine the number of smaller values
+// function whereIBelong(arr, num) {
+//    var counter = 0
+//    for (i = 0; i < arr.length; i++) {
+//        if (num > arr++[++i]) {
+//            counter++;
+//        }
+//    } 
+//    return counter
+// }
+                                                    // Using a While Loop
 function whereIBelong(arr, num) {
-   let i = 0
-   for(currNumber of arr){
-      if (num < currNumber){
-         console.log(` I Belong here ${i}`)
-         return i
-      }
-      i++
+   arr.sort((a, b) => {
+       return a - b
+   })
+   let counter = 0;
+   while (num > arr[counter]) {
+       counter++
    }
+   return counter
 }
+                                                    // Finding the index of the value directly
+// function whereIBelong(arr, num) {
+//    arr.push(num)
+//    arr.sort((a, b) => a - b)
+//    return arr.indexOf(num)
+// }
+
 
 
 
