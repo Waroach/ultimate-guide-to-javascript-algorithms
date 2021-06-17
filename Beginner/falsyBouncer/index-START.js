cbbde5e97f492e70cde64e47b96f9fb85af2bbdd
waroach <brent.rankin@gmail.com>
2021-06-17 12:51:53 -0700

    Finsihed falsyBouncer

--- Beginner/falsyBouncer/index-START.js
+++ Beginner/falsyBouncer/index-START.js
@@ -5,19 +5,37 @@ and return an array of only truthy values.
 E.g  falsyBouncer([1, 0, null, '', 5]) // should return [1,5]
 */
 
-// 1 failed 2 passed
+                                                    // First Try
+// function falsyBouncer(array) {
+//     let newArr = []
+//     array.forEach(e => {
+//         if (e == 0 || e == '' || e == undefined || e == false || e == null || e == NaN){
+//             console.log(`Fuck this ${e}`)
+//         }else{
+//             newArr.push(e)
+//         }
+//     })
+//     console.log(newArr)
+//     return newArr
+// }
+                                                    // Using a for…of loop
 function falsyBouncer(array) {
-    let newArr = []
-    array.forEach(e => {
-        if (e == 0 || e == '' || e == undefined || e == false || e == null || e == NaN){
-            console.log(`Fuck this ${e}`)
-        }else{
-            newArr.push(e)
+    let result =[]
+    //loop through with each array value
+    for (value of array){
+    // push into result if truthy
+        if(value){
+            result.push(value)
         }
-    })
-    console.log(newArr)
-    return newArr
+    }
+    return result
 }
+                                                    // Using .filter()
+// function falsyBouncer(array) {
+//     return array.filter((value) =>{
+//         return Boolean(value)
+//     })
+// }
 
 
 module.exports = falsyBouncer
