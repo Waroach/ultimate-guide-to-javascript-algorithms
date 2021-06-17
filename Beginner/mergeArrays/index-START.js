ce41765aa969f0ef30a7b96c15f1c9840cf1fb54
waroach <brent.rankin@gmail.com>
2021-06-17 12:36:18 -0700

    Finished mergeArrays

--- Beginner/mergeArrays/index-START.js
+++ Beginner/mergeArrays/index-START.js
@@ -4,23 +4,73 @@
  E.g mergeArrays([1,2,3,3,3], [1,4,5,2]) // should return [1,2,3,4,5]
 */
 
+                                                    // First Try all my code
 function mergeArrays(...arrays) {
-
-    let jointArray = []
-    
+    //concat the two arrays into one
+    let arrCon = []
     arrays.forEach(array => {
-        jointArray = [...jointArray, ...array]
-    });
-// ****************** START of My Code Same as chunkArray
-    let uniqueArray = jointArray.filter((c,i) => {
-        return jointArray.indexOf(c) === i
+        arrCon = [...arrCon, ...array]  
     })
-    console.log(uniqueArray)
-// ****************** END of My Code Same as chunkArray
-    return [...new Set([...jointArray])]
-
-    
+    // step thru arrCon
+    // place unigueitems into new Array
+    let uniqueItems = arrCon.filter((c, index) => {
+        return arrCon.indexOf(c) === index
+    })
+    return uniqueItems
 }
-
+                                                    // Second Try
+// function mergeArrays(...arrays) {
+//     let jointArray = []
+//     arrays.forEach(array => {
+//         jointArray = [...jointArray, ...array]
+//     });
+// // ****************** START of My Code Same as chunkArray
+//     let uniqueArray = jointArray.filter((c,i) => {
+//         return jointArray.indexOf(c) === i
+//     })
+//     console.log(uniqueArray)
+// // ****************** END of My Code Same as chunkArray
+//     return [...new Set([...jointArray])]
+// }
+                                                    // Using Sets set()
+// function mergeArrays(...arrays) {
+//     let jointArray = []
+//     arrays.forEach(array => {
+//         jointArray = [...jointArray, ...array]
+//     });
+//     return [...new Set([...jointArray])]
+// }
+                                                    // Using Sets from()
+// function mergeArrays(...arrays) {
+//     let jointArray = []
+//     arrays.forEach(array => {
+//         jointArray = [...jointArray, ...array]
+//     });
+//     return Array.from(new Set([...jointArray]))
+// }
+                                                    // Using .filter()
+// function mergeArrays(...arrays) {
+//     let jointArray = []
+//     arrays.forEach(array => {
+//         jointArray = [...jointArray, ...array]
+//     })
+//     const uniqueArray = jointArray.filter((item,index) => jointArray.indexOf(item) === index)
+//     return uniqueArray
+// }
+                                                    // Using .reduce()
+// function mergeArrays(...arrays) {
+//     let jointArray = []
+//     arrays.forEach(array => {
+//         jointArray = [...jointArray, ...array]
+//     })
+//     const uniqueArray = jointArray.reduce((newArray, item) =>{
+//         if (newArray.includes(item)){
+//             return newArray
+//         } else {
+//             return [...newArray, item]
+//         }
+//     }, [])
+//     return uniqueArray
+// }
 
 module.exports = mergeArrays
