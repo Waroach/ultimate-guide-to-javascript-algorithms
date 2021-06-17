d43358564499d6ccb59192145e2233e44afddd61
waroach <brent.rankin@gmail.com>
2021-06-16 18:05:57 -0700

    Finished isAnagram

--- Beginner/isAnagram/index-START.js
+++ Beginner/isAnagram/index-START.js
@@ -6,11 +6,11 @@ don't. E.g
 */
 
 
-
+                                                    // My First Try
 function isAnagram(stringA, stringB) {
     if(stringA.length === stringB.length){
-        let a = stringA.split('').sort().join('')
-        let b = stringB.split('').sort().join('')
+        let a = stringA.toLowerCase().split('').sort().join('')
+        let b = stringB.toLowerCase().split('').sort().join('')
         if(a === b){
             console.log('Is A Anagram')
             return true
@@ -23,6 +23,39 @@ function isAnagram(stringA, stringB) {
         return false
     }
 }
+                                                    // Direct comparison
+// function isAnagram(stringA, stringB){
+//     const sanitizeString = function (str) {
+//         return str.toLowerCase().replace(/[^a-z\d]/g, '').split('').sort().join('');
+//     }
+//     return sanitizeString(stringA) == sanitizeString(stringB)
+// }
+                                                    // Character Map Comparison
+// function isAnagram(stringA, stringB) {
+//     function createCharMap(text) {
+//         let charMap = {}
+//         for (let char of text) {
+//             if (charMap.hasOwnProperty(char)) {
+//                 charMap[char]++
+//             } else {
+//                 charMap[char] = 1
+//             }
+//         }
+//         return charMap
+//     }
 
+//     if (stringA.length === stringB.length) {
+//         let stringAMap = createCharMap(stringA)
+//         let stringBMap = createCharMap(stringB)
+//         for (let char in stringAMap) {
+//             if (stringAMap[char] !== stringBMap[char]) {
+//                 return false
+//             }
+//         }
+//         return true
+//     } else {
+//         return false
+//     }
+// }
 
 module.exports = isAnagram
